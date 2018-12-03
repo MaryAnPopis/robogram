2525f4b2ae44e0d35cd352b9bcb9c760c18e2c59
Mariam Domínguez <maryanpopis@hotmail.com>
2018-12-02 23:45:46 -0600

    feat(ProfileHeader) adding a profile header component

---
+++ src/views/Profile/ProfileHeader.js
@@ -0,0 +1,82 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+import globalVariables from "../../styles/variables";
+
+export const ProfileHeader = props => {
+  return (
+    <Styles.Profile className="container mt-3">
+      <Styles.Row className="row">
+        <div className="col-md-3" />
+        <div className="col-md-3">
+          <Styles.Img src={props.imgSrc} alt="" className="" />
+        </div>
+        <div className="col-md-3 mt-5">
+          <Styles.Username>
+            {`@${props.username}`}{" "}
+            <Styles.SecondaryButton
+              to={`/edit/${props.id}`}
+              className="btn btn-outline-dark "
+            >
+              Edit profile
+            </Styles.SecondaryButton>{" "}
+          </Styles.Username>
+        </div>
+        <div className="col-md-3" />
+      </Styles.Row>
+    </Styles.Profile>
+  );
+};
+
+ProfileHeader.propTypes = {
+  imgSrc: PropTypes.string,
+  username: PropTypes.string,
+  id: PropTypes.string
+};
+
+const Styles = {};
+
+Styles.Profile = styled.div``;
+
+Styles.Img = styled.img`
+  background: #a1ffce; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #faffd1,
+    #a1ffce
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #faffd1,
+    #a1ffce
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  clip-path: circle(60px at center);
+  margin-left: 10%;
+`;
+
+Styles.Username = styled.h1`
+  font-size: 32px;
+  color: ${globalVariables.darkBlue};
+  font-weight: 300;
+`;
+
+Styles.SecondaryButton = styled(Link)`
+  border-color: ${globalVariables.darkblue} !important;
+  border-width: 1px;
+  color: ${globalVariables.darkblue};
+  font-size: 12px;
+  text-align: center;
+  &:hover {
+    background-color: ${globalVariables.darkblue};
+  }
+  &:focus {
+    background-color: ${globalVariables.darkblue};
+  }
+`;
+
+Styles.Row = styled.div`
+  border-bottom: 1px solid rgb(167, 167, 167, 0.3);
+`;
